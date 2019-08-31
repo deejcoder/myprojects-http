ab20250cfef38cf259ad6392629a80755054884f
Dilicor <deejtonks@gmail.com>
2019-09-01 00:25:18 +1200

    change back button to something more aesthetic

--- src/components/CloseButton.js
+++ src/components/CloseButton.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Icon } from '@blueprintjs/core';
+import { Icon, Tooltip } from '@blueprintjs/core';
 
 
 export default class CloseButton extends React.Component {
     render() {
         return (
-            <div style={{ marginRight: 15, marginLeft: 15, marginTop: 15 }}>
-                <Link to="/">
-                    <Icon icon="cross" iconSize={25} />
-                </Link>
-            </div>
+            <Tooltip content="Back">
+                <div style={{ marginRight: 15, marginLeft: 15, marginTop: 15 }}>
+                    <Link to="/">
+                        <Icon icon="chevron-left" iconSize={35} />
+                    </Link>
+                </div>
+            </Tooltip>
         )
     }
 }
