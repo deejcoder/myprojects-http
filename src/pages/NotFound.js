43209d2ae1375cd399595717c937fe894ae7d8af
Dilicor <deejtonks@gmail.com>
2019-09-20 16:58:04 +1200

    update 404 NotFound page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,6 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
+import { H1, Button, Intent } from '@blueprintjs/core';
+import { PAGE_BACKGROUND, TEXT_COLOR } from '../const/colours';
+import { FaExclamationCircle } from 'react-icons/fa';
 
 
-const NotFound = () => <h1>404 - Resource not found</h1>
+const Content = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 36px;
+    color: ${TEXT_COLOR};
+    text-align: center;
 
-export default NotFound;
+    h1 {
+        text-align: center;
+        padding-left: 15px;
+        display: inline;
+        font-weight: 300;
+        color: ${TEXT_COLOR} !important;
+    }
+
+    p {
+        font-size: 16px;
+        line-height: 3.2;
+    }
+
+    a {
+        text-decoration: none;
+    }
+`
+
+export default class NotFound extends React.Component {
+
+    componentDidMount() {
+        // set body background color
+        document.body.style.backgroundColor = PAGE_BACKGROUND;
+    }
+
+    render() {
+        return (
+            <Content>
+                <FaExclamationCircle />
+                <H1>We couldn't find your page!</H1>
+                <p>Sorry about that, but the resource you are looking for could not be found.</p>
+                <a href="/">
+                    <Button intent={Intent.SUCCESS}>Go Home</Button>
+                </a>
+            </Content>
+        )
+    }
+}
